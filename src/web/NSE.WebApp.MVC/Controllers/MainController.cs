1ebfa1c1c851ad6f39778864f6e86c72a1f2156f
Quenedi <quenedisms@gmail.com>
2021-08-04 14:32:45 -0300

    03-APIdeCatalago

---
+++ src/web/NSE.WebApp.MVC/Controllers/MainController.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using NSE.WebApp.MVC.Models;
+
+namespace NSE.WebApp.MVC.Controllers
+{
+    public class MainController : Controller
+    {
+        protected bool ResponsePossuiErros(ResponseResult resposta)
+        {
+            if (resposta != null && resposta.Errors.Mensagens.Any())
+            {
+                foreach (var mensagem in resposta.Errors.Mensagens)
+                {
+                    ModelState.AddModelError(string.Empty, mensagem);
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ src/web/NSE.WebApp.MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,23 @@
+using System.Collections.Generic;
+
+namespace NSE.WebApp.MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public int ErroCode { get; set; }
+        public string Titulo { get; set; }
+        public string Mensagem { get; set; }
+    }
+
+    public class ResponseResult
+    {
+        public string Title { get; set; }
+        public int Status { get; set; }
+        public ResponseErrorMessages Errors { get; set; }
+    }
+
+    public class ResponseErrorMessages
+    {
+        public List<string> Mensagens { get; set; }
+    }
+}
